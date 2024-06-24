b08ea2833849fe8819e298d549d38b2eec2468ec
Dragnil2001 <zaripov19062016@gmail.com>
2024-06-24 17:29:02 +0300

    Создан проект "песочница"

---
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -0,0 +1,15 @@
+
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        hello("world");
+        hello("user");
+        hello("zero");
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+}
